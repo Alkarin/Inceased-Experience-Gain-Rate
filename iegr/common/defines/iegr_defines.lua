44db2f6f89e5f7580f25d9efdc5e9cfc73774bf2
Alexander Vaughan <avaughan11@gmail.com>
2019-10-21 17:36:31 -0400

    Update XP rate and add additional skill levels

--- iegr/common/defines/iegr_defines.lua
+++ iegr/common/defines/iegr_defines.lua
@@ -1,13 +1,13 @@
-NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 500;
-NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 500;
-NDefines.NMilitary.MAX_AIR_EXPERIENCE = 500;
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 5000;
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 5000;
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 5000;
 
-NDefines.NMilitary.UNIT_EXPERIENCE_PER_COMBAT_HOUR = 0.0001;
+NDefines.NMilitary.UNIT_EXPERIENCE_PER_COMBAT_HOUR = 0.00015;
 NDefines.NMilitary.UNIT_EXPERIENCE_SCALE = 1.0;
 NDefines.NMilitary.UNIT_EXPERIENCE_PER_TRAINING_DAY = 0.0015;
 
 NDefines.NMilitary.FIELD_EXPERIENCE_SCALE = 0.002;
-NDefines.NMilitary.FIELD_EXPERIENCE_MAX_PER_DAY = 3;				-- Most xp you can gain per day
+NDefines.NMilitary.FIELD_EXPERIENCE_MAX_PER_DAY = 6;				-- Most xp you can gain per day
 
 NDefines.NMilitary.EXPEDITIONARY_FIELD_EXPERIENCE_SCALE = 0.5;		-- reduction factor in Xp from expeditionary forces
 NDefines.NMilitary.LEND_LEASE_FIELD_EXPERIENCE_SCALE = 0.005;		-- Experience scale for lend leased equipment used in combat.
@@ -15,7 +15,9 @@ NDefines.NMilitary.LEADER_EXPERIENCE_SCALE = 1.0;
 
 NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.25;
 
-NDefines.NMilitary.EXPERIENCE_LOSS_FACTOR = 1.00;                 -- percentage of experienced solders who die when manpower is removed
+NDefines.NMilitary.EXPERIENCE_LOSS_FACTOR = 0.75;                 -- percentage of experienced solders who die when manpower is removed
 
 NDefines.NMilitary.BATALION_NOT_CHANGED_EXPERIENCE_DROP = 0.0;		-- Division experience drop if unit has same batalion
-NDefines.NMilitary.BATALION_CHANGED_EXPERIENCE_DROP = 0.5;			-- Division experience drop if unit has different batalion
+NDefines.NMilitary.BATALION_CHANGED_EXPERIENCE_DROP = 0.35;			-- Division experience drop if unit has different batalion
+
+NDefines.NMilitary.BASE_LEADER_TRAIT_GAIN_XP = 1.25;
